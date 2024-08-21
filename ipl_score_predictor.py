e4715cdcfdab8179e91926ed369901e86b5601f8
Vaibhavsingh Thakur <147599074+imvaibhavthakur@users.noreply.github.com>
2024-08-21 16:13:41 +0530

    Update ipl_score_predictor.py

--- ipl_score_predictor.py
+++ ipl_score_predictor.py
@@ -15,7 +15,7 @@ model = pickle.load(open(filename,'rb'))
 
 #Title of the page with CSS
 
-st.markdown("<h1 style='text-align: center; color: white;'> IPL Score Predictor 2024 </h1>", unsafe_allow_html=True)
+st.markdown("<h1 style='text-align: center; color: white;'> IPL ScoreWizard 2024 </h1>", unsafe_allow_html=True)
 
 #Add background image
 
