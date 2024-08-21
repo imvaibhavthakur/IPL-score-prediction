b978f02eaa7c001bdebf6be3945d0eecdeb5c3c2
Vaibhavsingh Thakur <147599074+imvaibhavthakur@users.noreply.github.com>
2024-08-21 14:31:01 +0530

    Delete script.py

--- script.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = https://drive.google.com/file/d/10Q7sLLI3dKHe2wL0KIrUM0KSaUFJyUz-/view?usp=sharing"
-response = requests.get(url)
-with open('ml_model.pkl', 'wb') as f:
-    f.write(response.content)
